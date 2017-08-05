d5e2c4061a8a4fd8e27760b5d520b314882c7a0d
Roey Shem Tov <roeyshemtov1@gmail.com>
2017-08-05 20:57:35 +0300

    Created Custom Dexter Exceptions

---
+++ src/main/scala/exceptions/DexterException.scala
@@ -0,0 +1,6 @@
+package exceptions
+
+/**
+  * Created by ShemTov on 05/08/2017.
+  */
+class DexterException(message: String) extends Exception(message)
---
+++ src/main/scala/exceptions/ImpalaQueryKillException.scala
@@ -0,0 +1,6 @@
+package exceptions
+
+/**
+  * Created by ShemTov on 05/08/2017.
+  */
+class ImpalaQueryKillException(message: String) extends DexterException(message)
